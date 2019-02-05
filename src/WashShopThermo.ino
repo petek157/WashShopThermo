de6bff0d383e95bda79bd3454cb3dfd165f67935
petek157 <peterkoruga@Peters-iMac.attlocal.net>
2019-02-05 16:12:08 -0600

    Lots of changes. Almost ready for first test build.

--- src/WashShopThermo.ino
+++ src/WashShopThermo.ino
@@ -44,13 +44,27 @@ struct HeatInfo {
   int resetCount;
 };
 
-//Averaging Variables
-// int tempArray[10];
-// int max;
-// int maxLocation;
-// int min;
-// int minLocation;
+int redPin = A3;
+int greenPin = D8;
+int bluePin = D4;
 
+//Start Value
+static enum { RED, BLUE } cColor;
+
+//Current LED Value
+int cLEDValue = 0;
+
+int ledSteps = 25;
+
+unsigned long lastMillis = 0;
+unsigned long delayMillis = 80;
+static enum { FADING_IN, FADING_OUT } led_state;
+
+int buttonPin = A1;
+int buttonState;
+int lastButtonState = HIGH;
+unsigned long lastDebounceTime = 0;
+unsigned long debounceDelay = 50;
 
 void setup() {
   Serial.begin();
@@ -64,11 +78,23 @@ void setup() {
   Particle.variable("tempOffset", tempOffset);
   Particle.variable("reset", resetCount);
 
+  Particle.variable("steps", ledSteps);
+  Particle.variable("delay", delayMillis);
+
   Particle.function("setHeatMode", setMode);
   Particle.function("setHighPoint", setHigh);
   Particle.function("setLowPoint", setLow);
   Particle.function("setOffset", setOffset);
   Particle.function("rsetreset", resetReset);
+
+  Particle.function("steps", setLedSteps);
+  Particle.function("delay", setdelay);
+
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+
+  pinMode(buttonPin, INPUT_PULLUP);
 
   pinMode(heatPin, OUTPUT);
   digitalWrite(heatPin, LOW);
@@ -82,6 +108,7 @@ void setup() {
     HeatInfo defaultObj = { 0, highSetPoint, lowSetPoint, tempOffset, heatMode, heatOn, resetCount};
     myObj = defaultObj;
     EEPROM.put(hAddr, myObj);
+    setColor(0, 255, 0); // green
   } else {
     highSetPoint = myObj.highSet;
     lowSetPoint = myObj.lowSet;
@@ -89,8 +116,10 @@ void setup() {
     heatMode = myObj.mode;
     if (heatMode == 1) {
       setPoint = lowSetPoint;
+      setColor(0, 0, 255); // blue
     } else if (heatMode == 2) {
       setPoint = highSetPoint;
+      setColor(255, 0, 0); // red
     }
     heatOn = myObj.status;
     resetCount = myObj.resetCount + 1;
@@ -108,85 +137,180 @@ void loop() {
    * - actually hook into heater board to check if they fire
    */
 
+  //toggleColor();
+
+  if (heatOn) {
+    unsigned long currentMillis = millis();
+
+  if ((currentMillis - lastMillis) > delayMillis) {
+    lastMillis = currentMillis;
+    switch (led_state) {
+      case FADING_IN:
+        Serial.println("*--- Fade In");
+        if ((cLEDValue + ledSteps) > 255) {
+          cLEDValue = 255;
+        } else {
+          cLEDValue = cLEDValue + ledSteps;
+        }
+
+        switch (cColor) {
+          case RED:
+            fadeColor(cLEDValue, 0, 0);
+            break;
+          case BLUE:
+            fadeColor(0, 0, cLEDValue);
+            break;
+          default:
+            break;
+        }
+
+        if (cLEDValue == 255) {
+          led_state = FADING_OUT;
+          break;
+        }
+        break;
+
+      case FADING_OUT:
+        Serial.println("*--- Fade Out");
+        if ((cLEDValue - ledSteps) < 1) {
+          cLEDValue = 0;
+        } else {
+          cLEDValue = cLEDValue - ledSteps;
+        }
+
+        switch (cColor) {
+          case RED:
+            fadeColor(cLEDValue, 0, 0);
+            break;
+          case BLUE:
+            fadeColor(0, 0, cLEDValue);
+            break;
+          default:
+            break;
+        }
+
+        if (cLEDValue == 0) {
+          led_state = FADING_IN;
+          break;
+        }
+        break;
+      default:
+      return;
+    }
+    
+  }
+
+  }
+
+  int reading = digitalRead(buttonPin);
+  if (reading != lastButtonState) {
+    // reset the debouncing timer
+    lastDebounceTime = millis();
+  }
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    // whatever the reading is at, it's been there for longer than the debounce
+    // delay, so take it as the actual current state:
+ 
+    // if the button state has changed:
+    if (reading != buttonState) {
+      buttonState = reading;
+      Serial.print("*--- Button ---* "); Serial.println(buttonState);
+      if (buttonState == LOW) {
+        if (heatMode == 0) {
+          setMode("1");
+        } else if (heatMode == 1) {
+          setMode("2");
+        } else {
+          setMode("0");
+        }
+      }
+    }
+  }
+  lastButtonState = reading;
+
   if (heatCounter%1200 == 0) {
     float _temp;
-    int   i = 0;
+    // int   i = 0;
 
-    do {
-      _temp = tempSensor.getTemperature();
-    } while (!tempSensor.crcCheck() && MAXRETRY > i++);
-
-    if (i < MAXRETRY) {
+    // do {
+    //   _temp = tempSensor.getTemperature();
+    // } while (!tempSensor.crcCheck() && MAXRETRY > i++);
+    _temp = tempSensor.getTemperature();
+    if (tempSensor.crcCheck()) {
+      
       if ( _temp > -10 && _temp < 38) {
         int thisTempF = tempSensor.convertToFahrenheit(_temp) + tempOffset;
+        
         avgTemp += thisTempF;
         avgCounter += 1; 
       }
-    } else {
-      Serial.println("Invalid reading");
     }
 
-      
-  }
-  heatCounter += 1;
-  if (avgCounter == 10) {
+    if (avgCounter == 10) {
                     
-    int tempReading = avgTemp/10;
+      int tempReading = avgTemp/10;
+      
+      if (tempReading != currentTemp) {
+        currentTemp = tempReading;
 
-    if (tempReading != currentTemp) {
-      currentTemp = tempReading;
-
-      if (heatMode == 0) {
-        if (currentTemp < 40) {
-        
-          if (sentWarning == false) {
-            /*
-            * Send alert to owner via text saying 
-            * "Temp is %temp in garage! Would you like me to turn the heat to the low set point?"
-            * responsing "Yes" to text will turn on the heat to the low setpoint
-            */
-            bool success;
-            success = Particle.publish("below-40", PRIVATE); //Setup to send text message to me
-            if (success) {
-              sentWarning = true;
-            } 
-          }
+        if (heatMode == 0) {
+          if (currentTemp < 40) {
           
+            if (sentWarning == false) {
+              /*
+              * Send alert to owner via text saying 
+              * "Temp is %temp in garage! Would you like me to turn the heat to the low set point?"
+              * responsing "Yes" to text will turn on the heat to the low setpoint
+              */
+              bool success;
+              success = Particle.publish("below-40", PRIVATE); //Setup to send text message to me
+              if (success) {
+                sentWarning = true;
+              } 
+            }
+            
+          }
+        } else {
+          checkToHeat();
         }
-      } else {
-        checkToHeat();
-      }
 
+        avgTemp = 0;
+        avgCounter = 0;
+        heatCounter = 0;
+      }
+    }
+
+    if (avgCounter > 10) {
       avgTemp = 0;
       avgCounter = 0;
       heatCounter = 0;
     }
+
   }
-  //if for some reason avgCounter get over 10 set to 0 and start again
-  if (avgCounter > 10) {
-    avgTemp = 0;
-    avgCounter = 0;
-    heatCounter = 0;
-  }
+  heatCounter += 1;
+
 }
 
 int setMode(String mode) {
-
+  Serial.print("Mode: "); Serial.println(mode);
   if(mode == "0") {
     digitalWrite(heatPin, LOW);
     heatOn = false;
     heatMode = 0;
+    setColor(0, 255, 0); // green
     writeLog();
     return 1;
   } else if (mode == "1") {
     setPoint = lowSetPoint;
     heatMode = 1;
+    setColor(0, 0, 255); // blue
     writeLog();
     checkToHeat();
     return 1;
   } else if (mode == "2") {
     setPoint = highSetPoint;
     heatMode = 2;
+    setColor(255, 0, 0); // red
     writeLog();
     checkToHeat();
     return 1;
@@ -201,6 +325,16 @@ void checkToHeat() {
       if (heatOn == true) {
         digitalWrite(heatPin, LOW);
         heatOn = false;
+        switch (cColor) {
+          case RED:
+            setColor(255, 0, 0);
+            break;
+          case BLUE:
+            setColor(0, 0, 255);
+            break;
+          default:
+            break;
+        }
       }
     }
 
@@ -208,6 +342,7 @@ void checkToHeat() {
       if (heatOn == false) {
         digitalWrite(heatPin, HIGH);
         heatOn = true;
+        led_state = FADING_OUT;
         //If being turned on after a warning and we can verify (future feature) that heat is on reset the warning
         if (sentWarning) {
           sentWarning = false;
@@ -245,5 +380,48 @@ int setLow(String temp) {
 int setOffset(String newOffset) {
   tempOffset = newOffset.toInt();
   writeLog();
+  return 1;
+}
+
+void setColor(int red, int green, int blue) {
+
+  if (red == 255) {
+    cColor = RED;
+  } else if (blue == 255) {
+    cColor = BLUE;
+  }
+
+  cLEDValue = 255;
+
+  red = 255 - red;
+  green = 255 - green;
+  blue = 255 - blue;
+
+  Serial.println("Set");
+  Serial.print(red); Serial.print(", "); Serial.print(green); Serial.print(", "); Serial.println(blue);
+
+  analogWrite(redPin, red); analogWrite(greenPin, green); analogWrite(bluePin, blue);
+}
+
+void fadeColor(int red, int green, int blue) {
+
+  red = 255 - red;
+  green = 255 - green;
+  blue = 255 - blue;
+
+  Serial.println("Fade");
+  Serial.print(red); Serial.print(", "); Serial.print(green); Serial.print(", "); Serial.println(blue);
+
+  analogWrite(redPin, red); analogWrite(greenPin, green); analogWrite(bluePin, blue);
+}
+
+int setLedSteps(String steps) {
+  ledSteps = steps.toInt();
+  return 1;
+}
+
+int setdelay(String delay) {
+  unsigned long newLong = delay.toInt();
+  delayMillis = newLong;
   return 1;
 }
